56848e49d1dc631ab9069c8722ec43467b3f8696
faraday58 <jarodriguezvera@gmail.com>
2026-09-23 20:37:29 -0600

    Add metodo Read

--- Matrix.cs
+++ Matrix.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 
 namespace WinFormsArreglosMultidimensionales_2027_I
 {
---
+++ Multidimentional.cs
@@ -0,0 +1,38 @@
+﻿
+namespace WinFormsArreglosMultidimensionales_2027_I
+{
+    internal class Multidimentional:Matrix
+    {
+        private float[,] A;
+
+        #region Constructores
+        public Multidimentional(int m,int n) : base()
+        {
+            M = m;
+            N = n;
+            A = new float[M, N];
+        }
+        #endregion
+
+        public static Multidimentional Read(string A)
+        {
+            //1 2 3
+            //4 5 6
+            //Matriz ingresada por el usuario
+            // 1,2,3;4,5,6
+            string[] rows = A.Split(';');
+            string[] columns = rows[0].Split(',');
+
+            Multidimentional matrix = new Multidimentional(rows.Length,columns.Length );
+            for(int i = 0; i < matrix.M; i++)
+            {
+                for(int j= 0; j < matrix.N; j++)
+                {
+                    matrix
+                }
+            }
+
+        }
+
+    }
+}
